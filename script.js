1edd916b5924c0d806adc06855a11ba03d25461b
Katarzyna-Bak <75198705+Katarzyna-Bak@users.noreply.github.com>
2021-08-29 10:48:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ function compute() {
         return;
     }
     else:
-    //Return the result message right below the interest
-    document.getElementById("result").innerHTML="If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>";
-    
-    //retrieve remaining values
-    var rate = parseFloat(document.getElementById("rate").value);
-    var years = parseInt(document.getElementById("years").value);
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear() + years;
+    {
+        //Return the result message right below the interest
+        document.getElementById("result").innerHTML="If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>";
+
+        //retrieve remaining values
+        var rate = parseFloat(document.getElementById("rate").value);
+        var years = parseInt(document.getElementById("years").value);
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear() + years;
+    }
 }
 
 //Add a function to show the slider values
